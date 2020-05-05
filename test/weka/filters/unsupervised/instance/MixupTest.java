e79667c35595dd3d89f365d780acd74467c77739
Cameron Salisbury <cam2772@gmail.com>
2020-05-05 15:50:59 +1200

    Improved beta distribution sample tests

--- test/weka/filters/unsupervised/instance/MixupTest.java
+++ test/weka/filters/unsupervised/instance/MixupTest.java
@@ -2,40 +2,56 @@ package weka.filters.unsupervised.instance;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Random;
-
-import static org.junit.Assert.assertTrue;
+import java.util.stream.Stream;
 
 public class MixupTest {
     @Test
     public void sampleBetaDistribution() {
-        final double a = 0.5;
-        final int times = 1000000;
-        List<Double> results = new ArrayList<>();
-        Random rnd = new Random(0);
+        double a = 1;
+        final long times = 1000000;
+        final double target = 2e-2;
+        final double binSize = 0.001;
+        final Random rnd = new Random(0);
 
-        // make samples
-        for (int i = 0; i < times; i++) {
-            results.add(Mixup.sampleBetaDistribution(a, rnd));
+        for (int n = 0; n < 10; n++) {
+            int[] bins = new int[(int) (1 / binSize)];
+            double[] errors = new double[(int) (1 / binSize)];
+            int timesTotal = 0;
+            a /= 2;
+
+            while (true) {
+                timesTotal += times;
+
+                double finalA = a;
+                Stream.generate(() -> Mixup.sampleBetaDistribution(finalA, rnd))
+                        .limit(times)
+                        .forEach((v) -> {
+                            if (v == 1.0) {
+                                bins[bins.length - 1]++;
+                                return;
+                            }
+                            bins[(int) (v / binSize)]++;
+
+                        });
+
+                int i = 0;
+                while (i * binSize < 1) {
+                    double finali = i;
+                    double expected = (weka.core.Statistics.incompleteBeta(a, a, (finali + 1) * binSize) - weka.core.Statistics.incompleteBeta(a, a, finali * binSize)) * timesTotal;
+                    double actual = bins[i];
+                    double error = (expected - actual) / expected;
+                    errors[i] = Math.abs(error);
+                    i++;
+                }
+
+                double maxError = Arrays.stream(errors).max().getAsDouble();
+                if (maxError < target) {
+                    System.out.println(a + ", " + timesTotal + ": " + maxError);
+                    break;
+                }
+            }
         }
-
-        double step = 0.01;
-        int i = 0;
-        List<Double> diffs = new ArrayList<>();
-
-        // calculate difference between expected count and actual count in each step size
-        while (i * step < 1) {
-            double finali = i;
-            double expected = weka.core.Statistics.incompleteBeta(a, a, (finali + 1) * step) - weka.core.Statistics.incompleteBeta(a, a, finali * step);
-
-            diffs.add(results.stream().filter(d -> d >= finali * step && d < (finali + 1) * step).count() / (double) times - expected);
-
-            i++;
-        }
-
-        // checks expected vs sample is sufficiently small
-        assertTrue(diffs.stream().noneMatch(d -> d >= 1e-3));
     }
 }
